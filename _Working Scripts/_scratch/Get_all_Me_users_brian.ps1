35e390c2eeba5e7bec4474111894065a0da3e48e
Sean Connealy <sjcnyc@gmail.com>
2019-04-22 12:28:56 -0400

    Added $Date & $CSVFile

--- _Working Scripts/_scratch/Get_all_Me_users_brian.ps1
+++ _Working Scripts/_scratch/Get_all_Me_users_brian.ps1
@@ -1,7 +1,10 @@
+$Date            = (get-date -f yyyy-MM-dd)
+$CSVFile         = "C:\Temp\AllUserMeReport_$($Date).csv"
+
 $getADUserSplat = @{
     Server     = "me.sonymusic.com"
     SearchBase = "DC=me,DC=sonymusic,DC=com"
     Filter     = '*'
     Properties = 'UserPrincipalname', 'SamAccountName', 'DistinguishedName', 'PasswordNeverExpires', 'PasswordNotRequired', 'Enabled', 'PasswordLastSet', 'PasswordExpired'
 }
-Get-ADUser @getADUserSplat |Select-Object $getADUserSplat.Properties |Export-Csv C:\Temp\All_Me_Users.csv -NoTypeInformation
+Get-ADUser @getADUserSplat |Select-Object $getADUserSplat.Properties |Export-Csv $CSVFile -NoTypeInformation
