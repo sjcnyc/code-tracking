fb68ec427ba889306928657c0c809c3ab6c7d4f6
Connealy <sconnea@bmg.bagint.com>
2019-07-01 12:19:08 -0400

    update home drive switch, use splatting

--- _Working Scripts/_scratch/renamehomedrivestest.ps1
+++ _Working Scripts/_scratch/renamehomedrivestest.ps1
@@ -1,5 +1,17 @@
-$Users = Get-ADUser -Filter {homedirectory -like "\\storage*home$*"} -Properties name, homedirectory, homedrive -Server 'me.sonymusic.com'
+$getADUserSplat = @{
+    Filter     = { homedirectory -like "\\storage*home$*" }
+    Properties = 'SamAccountName', 'HomeDirectory', 'HomeDrive'
+    Server     = 'me.sonymusic.com'
+}
+$Users = Get-ADUser @getADUserSplat
 
 foreach ($User in $Users) {
-  Set-ADUser -Identity $User -HomeDrive 'H:' -HomeDirectory "\\storage.me.sonymusic.com\$($User.SamAccountName)" -Server 'me.sonymusic.com' -WhatIf
+  $setADUserSplat = @{
+      HomeDrive     = 'H:'
+      HomeDirectory = "\\storage.me.sonymusic.com\$($User.SamAccountName)"
+      Server        = 'me.sonymusic.com'
+      WhatIf        = $true
+      Identity      = $User
+  }
+  Set-ADUser @setADUserSplat
 }
