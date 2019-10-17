313a9f296d5541b3fa1096850abe1ade31a01caf
Connealy <sconnea@bmg.bagint.com>
2019-10-17 15:13:20 -0400

    new script, get-onedriveSharedFiles

--- Office 365/Convert-EmailToSamName.ps1
+++ Office 365/Convert-EmailToSamName.ps1
@@ -3,7 +3,8 @@
     [CmdletBinding(SupportsShouldProcess)]
     Param(
         [string]$InputCSV,
-        [string]$OutputCSV
+        [string]$OutputCSV,
+        [string]$username
     )
 
     Import-Module -Name ActiveDirectory
@@ -11,22 +12,173 @@
     $result = New-Object System.Collections.ArrayList
 
     $props = @{
-        Properties = @('SamAccountName', 'GivenName', 'Surname', 'Mail')
+        Properties = @('SamAccountName', 'Mail')
     }
 
-    Import-Csv -Path $InputCSV |
-        Foreach-Object {
-        Get-ADUser -Filter { Mail -eq $_.Mail } @props | Select-Object $props.Properties
 
-        $info = [pscustomobject]@{
-            'GivenName'       = $_.GivenName
-            'Surname        ' = $_.Surname
-            'SamaccountaName' = $_.SamAccountName
-            'Mail'            = $_.Mail
-        }
-        $null = $result.Add($info)
-    }
-    $result | Export-Csv $OutputCSV -NoTypeInformation
+            $user = Get-ADUser -Filter { Mail -eq $username } @props -Server me.sonymusic.com | Select-Object $props.Properties
+
+            $info = [pscustomobject]@{
+                'originalName'    = $username
+                'SamaccountaName' = $user.SamAccountName
+                'Mail'            = $user.Mail
+            }
+            $null = $result.Add($info)
+
+    $result #| Export-Csv $OutputCSV -NoTypeInformation -Append
 }
 
-Get-SamFromEmail -InputCSV 'C:\Temp\RED User Emails.csv' -OutputCSV 'C:\Temp\RED_User_Alais.csv'
+$users =@"
+alex.brody@redmusic.com
+alicia.nelson@sonymusic.com
+adam.poch@redmusic.com
+abhijeet.shere.wns@sonymusic.com
+angie.troici@sonymusic.com
+kevin.beazer.peak@sonymusic.com
+barbara.farley.citadelny@sonymusic.com
+sangeeta.bhuyan@ap.sony.com
+brian.sherman@sonymusic.com
+beth.stopper@sonymusic.com
+herman.calloway.citadelny@sonymusic.com
+kulwinder.singh.itopia@sonymusic.com
+kulwinder.singh.itopia@sonymusic.com
+kulwinder.singh.itopia@sonymusic.com
+chena.life@sonymusic.com
+thomas.colello@redmusic.com
+cary.ryan@sonymusic.com
+candice.surrency@sonymusic.com
+kulwinder.singh.itopia@sonymusic.com
+carlos.tolentino.citadelny@sonymusic.com
+debbie.eisen@rcarecords.com
+donna.haynes@sonymusic.com
+davidv.smith@sonymusic.com
+david.steffens@pmgsonymusic.com
+dan.tousignant@redmusic.com
+eliana.espinosa@sonymusic.com
+joseph.fasulo.citadelny@sonymusic.com
+scott.finchler.itopia@sonymusic.com
+"kelly.fitzgerald@sonymusic.com	"
+jerry.flores.citadelny@sonymusic.com
+carlos.gamboa.citadelny@sonymusic.com
+askshay.ghosh.wns@sonymusic.com
+gerry.kuster@rcarecords.com
+Hallie.Swidler@sonymusic.com
+imran.hussain.peak@sonymusic.com
+babu.iismayil.itopia@sonymusic.com
+joe.chernik@sonymusic.com
+john.conway@sonymusic.com
+john.graziani@sonymusic.com
+joyce.lee@sonymusic.com
+joyce.moyik@sonymusic.com
+jacqueline.rottmann@sonymusic.com
+karenjoy.deang.citadelny@sonymusic.com
+karleen.esposito@sonymusic.com
+akman@theorchard.com
+keith.persaud@sonymusic.com
+kathy.schubach@sonymusic.com
+kulwinder.singh.itopia@sonymusic.com
+rachael.lee@sonymusic.com
+lisa.grauso@sonymusic.com
+lina.lozada@sonymusic.com
+miguel.lopez@sonymusic.com
+Lori.Weber@sonymusic.com
+sagar.madhiboina.wns@sonymusic.com
+sanskruti.margaj.wns@sonymusic.com
+michael.botta@sonymusic.com
+mike.gallagher@sonymusic.com
+kaim.mulla.wns@sonymusic.com
+neva.bray@sonymusic.com
+cecile.nunez@sonymusic.com
+bhavna.palav.wns@sonymusic.com
+biswajit.parida.itopia@sonymusic.com
+niko.petruzzella@sonymusic.com
+patricia.jackson@sonymusic.com
+peter.posimato@sonymusic.com
+patty.schreiber@sonymusic.com
+Parikshit.Sharma@ap.sony.com
+phil.zaks@sonymusic.com
+nikunj.rajgor.wns@sonymusic.com
+rose.evans@sonymusic.com
+shabista.rizvi.itopia@sonymusic.com
+Robert.Kordisch@redmusic.com
+ruben.robles@sonymusic.com
+richard.sager@sonymusic.com
+rick.sulling@sonymusic.com
+shabarish.sasidharan.itopia@sonymusic.com
+sunil.balkawade.wns@sonymusic.com
+steve.bickerton@sonymusic.com
+sharon.cardella@sonymusic.com
+sue.danz@sonymusic.com
+bharat.sharma.itopia@sonymusic.com
+Srinivasan.Murugesan@ap.sony.com
+emily.supercynski@sonymusic.com
+scott.wood@sonymusic.com
+alex.syner.citadelny@sonymusic.com
+alec.tebbenhoff@sonymusic.com
+terence.gonsalves@sonymusic.com
+trish.lubonski@sonymusic.com
+terry.mcgibbon@redmusic.com
+tuesday.ryan-jones@sonymusic.com
+kulwinder.singh.itopia@sonymusic.com
+vicki.snyder@sonymusic.com
+osama.waseem.ums@sonymusic.com
+gail.worthen@pmgsonymusic.com
+al.lam@sonymusic.com
+kevin.beazer.peak@sonymusic.com
+sangeeta.bhuyan@ap.sony.com
+brian.waslenko@sonymusic.com
+herman.calloway.citadelny@sonymusic.com
+lesley.callaghan@sonymusic.com
+anne.marie.carducci@sonymusic.com
+carrie.boyd@sonymusic.com
+kulwinder.singh.itopia@sonymusic.com
+kulwinder.singh.itopia@sonymusic.com
+kulwinder.singh.itopia@sonymusic.com
+kulwinder.singh.itopia@sonymusic.com
+cleve.meikle@sonymusic.com
+michelle.cordeiro@sonymusic.com
+chris.shepherd@sonymusic.com
+chris.smith@sonymusic.com
+maria.dsouza@sonymusic.com
+enza.sergi@sonymusic.com
+emily.stiver@sonymusic.com
+elaine.yam@sonymusic.com
+joseph.fasulo.citadelny@sonymusic.com
+scott.finchler.itopia@sonymusic.com
+carlos.gamboa.citadelny@sonymusic.com
+cynthia.gebrayel@sonymusic.com
+imran.hussain.peak@sonymusic.com
+babu.iismayil.itopia@sonymusic.com
+joel.duchesne@sonymusic.com
+jerry.flores@sonymusic.com
+jd.parent@sonymusic.com
+janet.tomkins@sonymusic.com
+karenjoy.deang.citadelny@sonymusic.com
+kulwinder.singh.itopia@sonymusic.com
+kulwinder.singh.itopia@sonymusic.com
+milijana.batricevic@sonymusic.com
+biswajit.parida.itopia@sonymusic.com
+nitin.patel_tcs@sonymusic.com
+Parikshit.Sharma@sonymusic.com
+paola.tataj@sonymusic.com
+chakrabarti.rudradip@sonydadc.com
+shabista.rizvi.itopia@sonymusic.com
+shabarish.sasidharan.itopia@sonymusic.com
+bharat.sharma.itopia@sonymusic.com
+michael.daniluk@sonymusic.com
+michael.daniluk@sonymusic.com
+michael.daniluk@sonymusic.com
+michael.daniluk@sonymusic.com
+Srinivasan.Murugesan@ap.sony.com
+alex.syner.citadelny@sonymusic.com
+tony.ramnauth@sonymusic.com
+trudy.sharples@sonymusic.com
+kulwinder.singh.itopia@sonymusic.com
+osama.waseem.ums@sonymusic.com
+wendy.hoskins@sonymusic.com
+yoko.king@sonymusic.com
+"@ -split [environment]::NewLine
+
+foreach ($user in $users) {
+    Get-SamFromEmail -username $user | Export-Csv D:\Temp\users_for_mike2.csv -NoTypeInformation -Append
+}
---
+++ _Working Scripts/Get-SharedOnedriveFiles.ps1
@@ -0,0 +1,74 @@
+using namespace System.Collections.Generic
+
+function Get-SharedOnedriveFiles {
+  param (
+    $UserPrincipalName
+  )
+
+  $Style1 =
+  '<style>
+  body {color:#333333;font-family:Calibri,Tahoma,arial,verdana;font-size: 10pt;}
+  h1 {text-align:center;}
+  h2 {border-top:1px solid #E9E9E9;}
+  h4 {font-size: 8pt;}
+  table {border-collapse:collapse;}
+  th {text-align:left;font-weight:bold;color:#FFFFFF;background-color:#2980B9;border:1px solid #2980B9;padding:4px;}
+  td {padding:4px; border:1px solid #E9E9E9;}
+  .odd { background-color:#F6F6F6; }
+  .even { background-color:#E9E9E9; }
+</style>'
+
+  $UserList = [List[PSObject]]::new()
+
+  $startDate = (get-date).AddDays(-30).ToString("MM/dd/yyyy")
+  $endDate = (get-date).AddDays(1).ToString("MM/dd/yyyy")
+
+  Try {
+
+    $results =
+    Search-UnifiedAuditLog -StartDate $startDate -EndDate $endDate -Operations "SharingSet" -UserIds $UserPrincipalName | Select-Object * -ExpandProperty AuditData | ConvertFrom-Json
+
+    foreach ($result in $results) {
+
+      $PSObj = [pscustomobject]@{
+        'UserID'         = $result.UserId
+        'CreationTime'   = $result.CreationTime
+        'Workload'       = $result.Workload
+        'File'           = $result.ObjectID -replace "https://sonymusicentertainment-my.sharepoint.com/personal/", ""
+        'SourceFileName' = $result.SourceFileName
+        'Target'         = $result.TargetUserOrGroupName
+      }
+      [void]$UserList.Add($PSObj)
+    }
+
+    #$UserList = $UserList | Sort-Object -Property File -Unique
+
+    $HTML = New-HTMLHead -title "OneDrive Shared Files" -style $Style1
+    $HTML += "<h3>UserName: $($result.UserId)</h3>"
+    $HTML += New-HTMLTable -inputObject $($UserList | Select-Object CreationTime, Workload, File, Target | Where-Object { $_.Workload -eq "OneDrive" })
+    $HTML += "<h4>Script Completed: $(Get-Date -Format G)</h4>" | Close-HTML
+
+    $EmailParams = @{
+      to         = "sconnea@sonymusic.com"
+      from       = 'PwSh Alerts pwshalerts@sonymusic.com'
+      subject    = 'OneDrive Shared Files'
+      smtpserver = 'cmailsony.servicemail24.de'
+      Body       = ($HTML | Out-String)
+      BodyAsHTML = $true
+    }
+
+    Send-MailMessage @EmailParams
+  }
+  catch {
+    $error.message
+  }
+}
+
+#Connect-MsolService
+
+$users = (Get-MsolUser -MaxResults 100 | Select-Object).UserPrincipalName
+
+foreach ($user in $users) {
+
+  Get-SharedOnedriveFiles -UserPrincipalName "brian.lynch@sonymusic.com"
+}
