5b728d03bff06dddd7242767f57a670e7744f258
Connealy <sean.connealy.peak@sonymusic.com>
2020-01-07 11:18:44 -0500

    beta changed upn script

---
+++ _Working Scripts/Add-TaskGroups_Bobby.ps1
@@ -0,0 +1,29 @@
+$TGroups =
+@"
+T2_ADM_AP_L_Modify_General_Tab
+T2_ADM_AP_L_Modify_Organization_Tab
+T2_ADM_AP_L_Modify_Profile_Tab
+T2_ADM_AP_L_Rename_User_Objects
+T2_ADM_EU_L_Modify_General_Tab
+T2_ADM_EU_L_Modify_Organization_Tab
+T2_ADM_EU_L_Modify_Profile_Tab
+T2_ADM_EU_L_Rename_User_Objects
+T2_ADM_LA_L_Modify_General_Tab
+T2_ADM_LA_L_Modify_Organization_Tab
+T2_ADM_LA_L_Modify_Profile_Tab
+T2_ADM_LA_L_Rename_User_Objects
+T2_ADM_NA_L_Modify_General_Tab
+T2_ADM_NA_L_Modify_Organization_Tab
+T2_ADM_NA_L_Modify_Profile_Tab
+T2_ADM_NA_L_Rename_User_Objects
+"@ -split [System.Environment]::NewLine
+
+try {
+
+  foreach ($TGroup in $TGroups) {
+    Add-ADGroupMember -Identity "T2_G_Global_AD_Operations-Role" -Members $_ -ErrorAction Stop -WhatIf
+  }
+}
+catch [Microsoft.ActiveDirectory.Management.ADException] {
+  $_.Exception.Message
+} # .....
---
+++ _Working Scripts/Get-ChangedUuserPrinciplaNames.ps1
@@ -0,0 +1,87 @@
+$Style1 =
+'<style>
+  body {color:#333333;font-family:Calibri,Tahoma,arial,verdana;font-size: 10pt;}
+  h1 {text-align:center;}
+  h2 {border-top:1px solid #E9E9E9;}
+  h4 {font-size: 8pt;}
+  table {border-collapse:collapse;}
+  th {text-align:left;font-weight:bold;color:#FFFFFF;background-color:#2980B9;border:1px solid #2980B9;padding:4px;}
+  td {padding:4px; border:1px solid #E9E9E9;}
+  .odd { background-color:#F6F6F6; }
+  .even { background-color:#E9E9E9; }
+</style>'
+
+$Date      = (get-date -f yyyy-MM-dd)
+$CSVFile   = "D:\Temp\UPNs_Changed_Last_day_$($Date).csv"
+$attachcsv = $false
+
+$ReferenceCSV = (Import-CSV "D:\Temp\Reference.csv").Where{ ![string]::IsNullOrWhiteSpace($_.userPrincipalName) -or $_.userPrincipalName -ne $null }
+
+$Lookup = $ReferenceCSV | Group-Object -AsHashTable -AsString -Property sAMAccountName
+
+$getADUserSplat = @{
+  Properties       = 'Name', 'sAMAccountName', 'userPrincipalName'
+  SearchBase       = "OU=STD,OU=Tier-2,DC=me,DC=sonymusic,DC=com"
+  LDAPFilter       = "(samAccountType=805306368)(!userAccountControl:1.2.840.113556.1.4.803:=2)"
+}
+
+Get-ADUser @getADUserSplat | Select-Object $getADUserSplat.Properties | Export-Csv D:\Temp\Difference.csv -NoTypeInformation
+
+$Results = Import-Csv -Path D:\Temp\Difference.csv | ForEach-Object {
+  $Samname = $_.sAMAccountName
+  if ($Lookup.ContainsKey($Samname)) {
+      $OldUPN = ($Lookup[$Samname]).userPrincipalName
+    }
+    else {
+      $OldUPN = "Unknown"
+    }
+    if ($_.userPrincipalName -ne $OldUPN -and $OldUPN -ne "Unknown") {
+      [pscustomobject]@{
+        sAMAccontName = $Samname
+        Name          = $_.Name
+        OldUPN        = $OldUPN
+        NewUPN        = $_.userPrincipalName
+      }
+    }
+  }
+
+  #$Results | Out-GridView
+if ($null -ne $Results ) {
+
+  $msg = "See Attached CSV Report"
+  $Count = $Results.Count
+  $Results | Export-Csv -Path $CSVFile -NoTypeInformation
+  $attachcsv = $true
+}
+else {
+  $msg   = ""
+  $Count = '0'
+}
+
+$InfoBody = [pscustomobject]@{
+  'Task'   = "Azure Hybrid Runbook Worker - Tier-2"
+  'Action' = "UPNs Changes in the Last Day"
+  "Count"  = $Count
+}
+
+$HTML =  New-HTMLHead -title "UPNs Changes in the Last Day" -style $Style1
+$HTML += New-HTMLTable -inputObject $(ConvertTo-PropertyValue -inputObject $InfoBody)
+$HTML += "<h4>$($Msg)</h4>"
+$HTML += "<h4>Script Completed: $(Get-Date -Format G)</h4>" | Close-HTML
+
+$EmailParams = @{
+  to         = "sconnea@sonymusic.com", "Alex.Moldoveanu@sonymusic.com"
+  from       = 'PwSh Alerts pwshalerts@sonymusic.com'
+  subject    = 'UPNs Changes in the Last Day'
+  smtpserver = 'cmailsony.servicemail24.de'
+  Body       = ($HTML | Out-String)
+  BodyAsHTML = $true
+}
+if ($attachcsv) {
+  Send-MailMessage @EmailParams -Attachments $CSVFile
+}
+else {
+  Send-MailMessage @EmailParams
+}
+
+#TODO remove all csv file at this point, and export $Results object as Reference.csv...
