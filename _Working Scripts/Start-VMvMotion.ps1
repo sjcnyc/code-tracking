86e84df7e00388185943ce65fe6a3a60b017c2ed
Connealy <sean.connealy.peak@sonymusic.com>
2020-03-05 16:10:55 -0500

    code updates

--- _Working Scripts/Start-VMvMotion.ps1
+++ _Working Scripts/Start-VMvMotion.ps1
@@ -4,32 +4,35 @@
 #vm_1
 #vm_2
 
+<# $csvinput = @"
+vm_1
+vm_2
+"@-split [environment]::NewLine #>
+
+
 $csvinput = "path to csv"
 $maxsession = 2
 $runsession = 2
 targetds = "target_datastore"
 
-import-csv $csvinput | ForEach-Object {
+Import-Csv  $csvinput | ForEach-Object {
 
   Write-Output "Checking the running vMotion..."
 
-  Do {
-
+  do {
     $runsession = (get-task | Where-Object { $_.name -like "RelocateVM_Task" -and $_.State -eq "Running" }).count
 
     if ($runsession -ge $maxsession) {
       Write-Output "The current running vMotion sessions is $($runsession.No) new vMotion will be started.  Next check will be performed in 1 minute."
-      Start-Sleep -s 60
+      Start-Sleep -Seconds 60
       get-task | Where-Object { $_.State -eq "running" }
     }
-
     else {
       Write-Output "The current running vMotion sessions is $($runsession), a new storage vMotion will be started soon."
-      Start-Sleep -s 5
+      Start-Sleep -Seconds 5
     }
-
-  } While ( $runsession -ge $maxsession)
-
+  }
+  while ( $runsession -ge $maxsession)
 
   Write-Output ""
   Write-Output "The Storage vMotion for will start for below VM..."
