a12a632b0ae101e52b143178342599063b1aac6b
Sean Connealy <sjcnyc@gmail.com>
2020-05-30 16:22:06 -0400

    file clean up

--- _Working Scripts/ComputerInfo_gather.ps1
+++ _Working Scripts/ComputerInfo_gather.ps1
@@ -1,12 +1,28 @@
 using namespace System.Collections.Generic
 
 $List = [List[PSObject]]::new()
+$jsonBase = @{ }
 
-$mappings = Get-SmbMapping
+$mappings = (Get-SmbMapping).Where{ $_.status -eq "OK" -and (![String]::IsNullOrWhiteSpace($_.LocalPath)) }
+$computerInfo = Get-ComputerInfo
+
+foreach ($comp in $computerInfo) {
+  $psObject = [pscustomobject]@{
+    HostInfo =
+    [pscustomobject]@{
+      Hostname    = $comp.CsDNSHostName
+      Domain      = $comp.CsDomain
+      LogonServer = $comp.LogonServer
+      ProductName = $comp.WindowsProductName
+      OSVersion   = $comp.OsVersion
+    }
+  }
+  [void]$List.Add($psObject)
+}
 
 foreach ($mapping in $mappings) {
   $psObject = [pscustomobject]@{
-    Mapping =
+    Mappings =
     [pscustomobject]@{
       Drive = $mapping.LocalPath
       Path  = $mapping.RemotePath
@@ -15,19 +31,17 @@ foreach ($mapping in $mappings) {
   [void]$List.Add($psObject)
 }
 
-$computerInfo = Get-ComputerInfo
+$users = @{"Users" = $List;}
+$jsonBase.Add("Data", $users)
+$jsonBase | ConvertTo-Json -Depth 10
 
-foreach ($comp in $computerInfo) {
-  $psObject = [pscustomobject]@{
-    Hostname    = $comp.CsDNSHostName
-    Domain      = $comp.CsDomain
-    LogonServer = $comp.LogonServer
-    ProductName = $comp.WindowsProductName
-    OSVersion   = $comp.OsVersion
 
+<# $Arr = foreach ($Object in $mappings) {
+  $copy = [PSCustomObject]::new()
+  $Object.PSObject.Properties | ForEach-Object {
+    $copy | Add-Member -MemberType NoteProperty -Name $_.Name -Value $_.Value
   }
-  [void]$List.Add($psObject)
+  $copy
 }
 
-
-$List | ConvertTo-Json
+$Arr #>
