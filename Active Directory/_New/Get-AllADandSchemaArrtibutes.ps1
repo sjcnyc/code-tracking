213c09fe3d84cb559163376a99536b80e2143ee3
Connealy <sean.connealy.peak@sonymusic.com>
2020-06-07 18:17:27 -0400

    script updates

---
+++ Active Directory/_New/Get-AllADandSchemaArrtibutes.ps1
@@ -0,0 +1,17 @@
+# Import the Active Directory module:
+Import-Module ActiveDirectory
+
+# Now, obtain a reference to the assembly itself:
+$ADAssembly = [Microsoft.ActiveDirectory.Management.ADEntity].Assembly
+
+# Now we'll need to retrieve the internal class that defines the constants:
+$LDAPAttributes = $ADAssembly.GetType('Microsoft.ActiveDirectory.Management.Commands.LdapAttributes')
+
+# Then use GetFields() to retrieve the internal constants
+$LDAPNameConstants = $LDAPAttributes.GetFields('Static,NonPublic') | Where-Object { $_.IsLiteral }
+
+# Finally build a hashtable with the Property Names -> LDAP Name mapping
+$LDAPPropertyMap = @{ }
+$LDAPNameConstants | ForEach-Object {
+  $LDAPPropertyMap[$_.Name] = $_.GetRawConstantValue()
+}
--- MSGraph/Set-UserAccountExtensionDate.ps1
+++ MSGraph/Set-UserAccountExtensionDate.ps1
@@ -1,3 +1,6 @@
+using namespace System.Collections.Generic
+$userList = [List[PSObject]]::new()
+
 $connectGraphSplat = @{
   ClientId              = "91152ce4-ea23-4c83-852e-05e564545fb9"
   TenantId              = "f0aff3b7-91a5-4aae-af71-c63e1dda2049"
@@ -5,25 +8,31 @@ $connectGraphSplat = @{
 }
 Connect-Graph @connectGraphSplat
 
+$userList = [List[PSObject]]::new()
+
 $getMessageSplat = @{
   UserId = '1dcf9218-b0ae-450f-9cba-c8446edf8cc2'
   Filter = "contains(subject,'Extend')"
 }
 
-$users = Get-MgUserMessage @getMessageSplat | Select-Object bodycontent, ID
+$users = Get-MgUserMessage @getMessageSplat | Select-Object -ExpandProperty bodycontent
 
 $regEx = 'username:\s+(.*)[\s\S]+?' + 'extensiondate:\s+(.*)[\s\S]+?'
 
 foreach ($user in $users) {
 
-  $objArr = @()
-  $objArr = ($user | Out-String) |
+  $user | Out-String |
   Where-Object { $_ -match $regEx } |
   ForEach-Object {
-    [pscustomobject] ([ordered]@{
-        UserName   = $matches[1]
-        ExtendDate = $matches[2]
-      })
+    $PSObj = [pscustomobject]@{
+      UserName   = $matches[1]
+      ExtendDate = $matches[2]
+    }
+    [void]$userList.Add($PSObj)
   }
-  $objArr
 }
+
+$userList | Select-Object username
+
+
+# ? Get-ADUser -filter "employeeNumber -eq ""$Currentid"""
