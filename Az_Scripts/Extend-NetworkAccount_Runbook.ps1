32dcca8cbd8fea2654782764591f74154d995314
Connealy <sean.connealy.peak@sonymusic.com>
2020-08-19 08:33:52 -0400

    Added 2nd error email

--- Az_Scripts/Extend-NetworkAccount_Runbook.ps1
+++ Az_Scripts/Extend-NetworkAccount_Runbook.ps1
@@ -1,23 +1,26 @@
+
 Import-Module Az
 $ErrorActionPreference = "Continue"
 $ADUser = $null
 $LocalXMLPath = "D:\ExtNetAcctLogs"
+#$LocalXMLPath = "\\storage.me.sonymusic.com\wwinfra$\ExtNetAcctLogs"
 $TicketPath = "$($LocalXMLPath)\Tickets"
 $LogPath = "$($LocalXMLPath)\Logs"
 $LogName = "UpdatedAccountsLog.csv"
 $ConnectionString = "DefaultEndpointsProtocol=https;EndpointSuffix=core.windows.net;AccountName=sa01sftx3406815508;AccountKey=2u6z1WymVz3kYLZZa+xHsLdZSvDYQLXVMN/V27TMUgQe6XKE2bUYyVySBjvC5ps0XHo/nsqSRjufyScGVg7f4Q=="
 $SourceStorageContext = New-AzStorageContext -ConnectionString $ConnectionString
 $SrcContainer = "con01-sf-dropoff"
-#$DestContainer         = "con01-sf-archive"
+$DestContainer = "con01-sf-archive"
 $Cred = Get-AutomationPSCredential -Name 'T2_Cred'
 
-$Blobs = Get-AzStorageBlob -Container $SrcContainer -Context $SourceStorageContext | Where-Object { $_.name -like "00*" }
+Write-Output "Getting XML Files..."
+$Blobs = Get-AzStorageBlob -Container $SrcContainer -Context $SourceStorageContext #| Where-Object { $_.name -like "00*" }
 
 if ($Blobs) {
 
   foreach ($Blob in $Blobs) {
 
-    Write-Output $Blob.Name
+    Write-Output "Processing: $($Blob.Name)"
 
     $getAzStorageBlobContentSplat = @{
       Blob        = $Blob.Name
@@ -27,8 +30,9 @@ if ($Blobs) {
     }
 
     Get-AzStorageBlobContent @getAzStorageBlobContentSplat -Force
+    Write-Output "Copying: $($Blob.Name) to: $($LocalXMLPath)"
 
-    <#     $startAzStorageBlobCopySplat = @{
+    $startAzStorageBlobCopySplat = @{
       SrcBlob       = $Blob.Name
       Context       = $SourceStorageContext
       SrcContainer  = $SrcContainer
@@ -36,89 +40,123 @@ if ($Blobs) {
       DestBlob      = $Blob.Name
     }
 
-    Start-AzStorageBlobCopy @startAzStorageBlobCopySplat -Force #>
+    #Start-AzStorageBlobCopy @startAzStorageBlobCopySplat -Force
 
     Get-AzStorageBlob -Context $SourceStorageContext -Container $SrcContainer -Blob $Blob.Name | Remove-AzStorageBlob
+    Write-Output "Removing: $($Blob.Name)"
 
-    $LocalXMLFiles = Get-ChildItem -Path $LocalXMLPath -File -Filter *.xml
+    # $LocalXMLFiles = Get-ChildItem -Path $LocalXMLPath -File -Filter *.xml
+    $LocalXMLFile = Get-ChildItem -Path $LocalXMLPath -File -Filter $Blob.Name
+    Write-Output "Processing: $($LocalXMLFile)"
 
-    foreach ($XMLFile in $LocalXMLFiles) {
+    #foreach ($XMLFile in $LocalXMLFiles) {
 
-      $TicketXML = [Xml] (Get-Content -Path "$($XMLFile.FullName)")
+    $TicketXML = [Xml] (Get-Content -Path "$($LocalXMLFile.FullName)")
 
-      try {
+    try {
 
-        $UserId = $TicketXML.userAction.userId
+      $UserId = $TicketXML.userAction.userId
 
-        $ADUser = Get-ADUser -Identity $UserId -Properties DistinguishedName, sAMAccountName, accountExpires, Name | 
-        Select-Object DistinguishedName, sAMAccountName, Name, @{N = "ExpiryDate"; E = { [datetime]::FromFileTime($_.accountExpires) } }
+      $ADUser = Get-ADUser -Identity $UserId -Properties DistinguishedName, sAMAccountName, accountExpires, Name | 
+      Select-Object DistinguishedName, sAMAccountName, Name, @{N = "ExpiryDate"; E = { [datetime]::FromFileTime($_.accountExpires) } }
+      Write-Output "Getting properties for: $($ADUser.samaccountname)"
+    }
+    catch {
+
+      Write-Output "$($Error[0].Exception.Message)"
+        
+      if ($null -eq $ADUser.samaccountname) {
+        $UserId = "N/A"
       }
-      catch {
 
-        Write-Output "$($Error[0].Exception.Message)"
-
-        $Body = @"
+      $Body = @"
 <pre>
-Username: $($UserId)
+UserId:   $($UserId)
+Ticket#:  $($TicketXML.userAction.ticketnumber)
+Error:    $($Error[0].Exception.Message)
+</pre>
+"@
+
+      $Body2 = @"
+<pre>
+UserId:   $($UserId)
 Ticket#:  $($TicketXML.userAction.ticketnumber)
 RefId:    $($TicketXML.userAction.RefId)
 Error:    $($Error[0].Exception.Message)
 </pre>
 "@
-        $EmailParams = @{
-          to         = "sconnea@sonymusic.com"
-          from       = 'PwSh Alerts pwshalerts@sonymusic.com'
-          subject    = "Account access not extended. Ticket# $($TicketXML.userAction.ticketnumber)"
-          smtpserver = 'cmailsony.servicemail24.de'
-          body       = ($Body | Out-String)
-          bodyashtml = $true
-        }
-
-        Send-MailMessage @EmailParams 3>$null
-        Write-Output "Sending error email, user: $($UserId)"
-        $ADUser = $null
+      $EmailParams = @{
+        to         = "sconnea@sonymusic.com", "Salesforce.Requests@sonymusic.com"
+        from       = 'PwSh Alerts pwshalerts@sonymusic.com'
+        subject    = "Account access not extended. Ticket# $($TicketXML.userAction.ticketnumber)"
+        smtpserver = 'cmailsony.servicemail24.de'
+        body       = ($Body | Out-String)
+        bodyashtml = $true
       }
 
-      if ($ADUser) {
+      Send-MailMessage @EmailParams 3>$null
 
-        $ExpiresDate = [datetime]::parseexact((($TicketXML.userAction).expdate), 'yyyy-MM-dd HH:mm:ss', $null).ToString('MM/dd/yyyy HH:mm:ss tt')
+      $EmailParams2 = @{
+        to         = "sconnea@sonymusic.com", "salesforce.requests@4-umsi1z03v0oznmvjd6i9do7wep3916t0e9wojknoc39w61pd0.7-jlcaeao.na33.case.salesforce.com"
+        from       = 'PwSh Alerts pwshalerts@sonymusic.com'
+        subject    = "Account access not extended. Ticket# $($TicketXML.userAction.ticketnumber)"
+        smtpserver = 'cmailsony.servicemail24.de'
+        body       = ($Body2 | Out-String)
+        bodyashtml = $true
+      }
 
-        Set-ADAccountExpiration -Identity $($ADUser.DistinguishedName) -DateTime $ExpiresDate -Credential $Cred
+      Send-MailMessage @EmailParams2 3>$null
+      Write-Output "Sending error email, user: $($UserId)"
+      $ADUser = $null
+    }
 
-        $Body = @"
+    if ($ADUser) {
+
+      $ExpiresDate = [datetime]::parseexact((($TicketXML.userAction).expdate), 'yyyy-MM-dd HH:mm:ss', $null).ToString('MM/dd/yyyy HH:mm:ss tt')
+
+      Set-ADAccountExpiration -Identity $($ADUser.DistinguishedName) -DateTime $ExpiresDate -Credential $Cred
+
+      $NewAccountexpires = Get-ADUser -Identity $UserId -Properties accountExpires, Name | 
+      Select-Object @{N = "ExpiryDate"; E = { [datetime]::FromFileTime($_.accountExpires) } }
+      Write-Output "Updating expiry date for: $($UserId)"
+
+      $Body = @"
 <pre>
-Username:    $($UserId)
+UserId:      $($UserId)
 Ticket#:     $($TicketXML.userAction.ticketnumber)
 RefId:       $($TicketXML.userAction.RefId)
-ExpiresDate: $($ExpiresDate)
+ExpiresDate: $(($NewAccountexpires.ExpiryDate).ToString('MM/dd/yyyy HH:mm:ss tt')) 
 </pre>
 "@
-        $EmailParams2 = @{
-          to         = "sconnea@sonymusic.com"
-          from       = 'PwSh Alerts pwshalerts@sonymusic.com'
-          subject    = "Account access extended. Ref# $($TicketXML.userAction.RefId)"
-          smtpserver = 'cmailsony.servicemail24.de'
-          body       = ($Body | Out-String)
-          bodyashtml = $true
-        }
-
-        Send-MailMessage @EmailParams2 3>$null
-        Write-Output "Sending success email, user: $($UserId)"
-      
-        $UserObject = [pscustomobject]@{
-          Name              = $ADUser.Name
-          SamAccountName    = $ADUser.sAMAccountName
-          DistinguishedName = $ADUser.DistinguishedName
-          AccountExpires    = ($ADUser.ExpiryDate).ToString('MM/dd/yyyy HH:mm:ss tt') 
-          NewAccountExpires = $ExpiresDate 
-          Scriptexecution   = (Get-Date).ToString('MM/dd/yyyy HH:mm:ss tt')
-        }
-
-        $UserObject | Export-Csv "$($LogPath)\$($LogName)" -NoType -Append
-
-        $ADUser = $null
+      $EmailParams3 = @{
+        to         = "sconnea@sonymusic.com", "extend.account@sonymusic.com", "salesforce.requests@4-umsi1z03v0oznmvjd6i9do7wep3916t0e9wojknoc39w61pd0.7-jlcaeao.na33.case.salesforce.com"
+        from       = 'PwSh Alerts pwshalerts@sonymusic.com'
+        subject    = "Account access extended. Ref# $($TicketXML.userAction.RefId)"
+        smtpserver = 'cmailsony.servicemail24.de'
+        body       = ($Body | Out-String)
+        bodyashtml = $true
       }
+
+      Send-MailMessage @EmailParams3 3>$null
+      Write-Output "Sending success email, user: $($UserId)"
+      
+      $UserObject = [pscustomobject]@{
+        Name              = $ADUser.Name
+        SamAccountName    = $ADUser.sAMAccountName
+        DistinguishedName = $ADUser.DistinguishedName
+        AccountExpires    = ($ADUser.ExpiryDate).ToString('MM/dd/yyyy HH:mm:ss tt') 
+        NewAccountExpires = ($NewAccountexpires.ExpiryDate).ToString('MM/dd/yyyy HH:mm:ss tt') 
+        Scriptexecution   = (Get-Date).ToString('MM/dd/yyyy HH:mm:ss tt')
+      }
+
+      $UserObject | Export-Csv "$($LogPath)\$($LogName)" -NoType -Append
+      "Updating Log: $($LogPath)\$($LogName)"
+
+      $ADUser = $null
     }
-    Move-Item -Path "$($LocalXMLPath)\$($Blob.Name)" -Destination "$($TicketPath)\$($Blob.Name)" -Force
+    Move-Item -Path "$($LocalXMLFile.FullName)" -Destination "$($TicketPath)\$($LocalXMLFile.Name)" -Force
+    Write-Output "Moving: $($LocalXMLFile.Name) to: $($TicketPath)\$($LocalXMLFile.Name)"
+    #}
   }
+  Write-Output "Loop Complete."
 }
