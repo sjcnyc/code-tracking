a56ce847b1fc9224670e2a1c500864f4d18cd4a7
Sean Connealy <sjcnyc@gmail.com>
2020-08-25 10:41:39 -0400

    First commit

---
+++ Working_Scripts/New-ScriptFileInfo_Sean.ps1
@@ -0,0 +1,31 @@
+﻿$Parms = @{
+  Path                       = "D:\POWERSHELL\Powershell-Scripts\Working_Scripts\GetSysInfo2.ps1"
+  Verbose                    = $True
+  Version                    = "1.4.0"
+  Author                     = "sconnea@sonymusic.com.com"
+  Description                = "Get SystemInformation"
+  CompanyName                = "Sony Music"
+  Copyright                  = "2020 Sony Music. All rights reserved."
+  ExternalModuleDependencies = "ff", "bb"
+  RequiredScripts            = "Start-WFContosoServer", "Stop-ContosoServerScript"
+  ExternalScriptDependencies = "Stop-ContosoServerScript"
+  Tags                       = @("Tag1", "Tag2", "Tag3")
+  ProjectUri                 = "https://contoso.com"
+  LicenseUri                 = "https://contoso.com/License"
+  IconUri                    = "https://contoso.com/Icon"
+  PassThru                   = $True
+  ReleaseNotes               = @("Contoso script now supports the following features:",
+    "Feature 1",
+    "Feature 2",
+    "Feature 3",
+    "Feature 4",
+    "Feature 5")
+  RequiredModules            =
+  "1",
+  "2",
+  "RequiredModule1",
+  @{ModuleName = "RequiredModule2"; ModuleVersion = "1.0" },
+  @{ModuleName = "RequiredModule3"; RequiredVersion = "2.0" },
+  "ExternalModule1"
+}
+New-ScriptFileInfo @Parms
