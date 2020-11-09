e9a579fefef5970b2824a6e72dc93944d056ebac
Connealy <sean.connealy.peak@sonymusic.com>
2020-11-09 17:52:44 -0500

    user name changes

--- Working_Scripts/Upload-OneDrive_ShareGate.ps1
+++ Working_Scripts/Upload-OneDrive_ShareGate.ps1
@@ -1,15 +1,16 @@
-$remoteSite = "Miami"
-$migrationFilePath = "D:\Temp\OneDrive_Migration"
-$regionHomeDrives = "$($remoteSite)_Home_drives.csv"
-$ODUsersFile = "OD_Users_Main.csv"
+$remoteSite        = "Miami"
+$migrationFilePath = "D:\OneDrive_Migration"
+$regionHomeDrives  = "$($remoteSite)_Home_drives.csv"
+$ODUsersFile       = "OD_Users_Main.csv"
 
 $results = @()
 
 $users = @"
-Rcottec
-POMB002
-MDIAZ2
-SCADENA
+avila04
+ARANO01
+ANGE056
+AANE001
+sanch03
 "@ -split [environment]::NewLine
 
 foreach ($user in $users) {
@@ -17,8 +18,8 @@ foreach ($user in $users) {
     $getuser = Get-ADUser $user -Properties DisplayName, sAMAccountName | Select-Object DisplayName, sAMAccountName
 
     $results += [pscustomobject]@{
-      DisplayName = $getuser.DisplayName
-      HomeDirectory = "D:\production_shares\Users\$($user)"
+      DisplayName    = $getuser.DisplayName
+      HomeDirectory  = "D:\production_shares\Users\$($user)"
       SamAccountName = $getuser.sAMAccountName
     }
   }
@@ -28,15 +29,15 @@ foreach ($user in $users) {
 }
 $results | Export-Csv "$($migrationFilePath)\$($remoteSite)\$($regionHomeDrives)"
 
-$AllUsers = Import-Csv "$($migrationFilePath)\$($ODUsersFile)"
+$AllUsers  = Import-Csv "$($migrationFilePath)\$($ODUsersFile)"
 $HomeUsers = Import-Csv "$($migrationFilePath)\$($remotesite)\$($regionHomeDrives)"
 
 Join-Object -Left $AllUsers -Right $HomeUsers -LeftJoinProperty DisplayName1 -RightJoinProperty DisplayName |
-Export-Csv D:\Temp\OneDrive_Migration\Miami\OD_Migration_Joined.csv
+Export-Csv D:\OneDrive_Migration\Miami\OD_Migration_Joined.csv
 
 
 
-Import-Module Sharegate
+<# Import-Module Sharegate
 $csvFile    = "C:\MigrationPlanning\onedrivemigration.csv"
 $table      = Import-Csv $csvFile -Delimiter ","
 $mypassword = ConvertTo-SecureString "mypassword" -AsPlainText -Force
@@ -50,4 +51,4 @@ foreach ($row in $table) {
   Import-Document -SourceFolder $row.DIRECTORY -DestinationList $dstList
   Remove-SiteCollectionAdministrator -Site $dstSite
   Export-Report "$($migrationFilePath)\$($remotesite)"
-}
+} #>
