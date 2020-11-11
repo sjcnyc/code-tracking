c13694245540c9bc0869731533f78cab2cb0c06f
Connealy <sean.connealy.peak@sonymusic.com>
2020-11-11 12:43:59 -0500

    Convert 2 advanced function  Add ValueFromPipeline

--- Working_Scripts/Get-GPPReport_Kim.ps1
+++ Working_Scripts/Get-GPPReport_Kim.ps1
@@ -1,33 +1,41 @@
 function Get-GPPReport {
+  [CmdletBinding()]
   param (
+    [parameter(ValueFromPipeline = $true, Mandatory = $true, Position = 0)]
+    [string[]]
+    $gppName,
 
-    [string]
-    $GPPName
+    [parameter(Mandatory = $true, Position = 1)]
+    [string]$path,
+
+    [parameter(Mandatory = $true, Position = 2)]
+    [string]$fileName
   )
 
-  $GPPName | ForEach-Object {
+  begin { $fileName = "$($fileName)_$(Get-Date -Format {MM-d-yyyy}).csv" }
 
-    $GUID = (Get-GPO -Name $_).Id
-
-    [xml]$GpoXml = Get-GPOReport -Guid $GUID.Guid -ReportType Xml
-
-    foreach ($setting in $GpoXml.GPO.Computer.ExtensionData.Extension.LocalUsersAndGroups.Group) {
-
-      foreach ($user in $setting.properties.members.member.name) {
-
-        foreach ($computer in $setting.Filters.FilterComputer.name) {
-
-          [pscustomobject]@{
-            GPO       = $GpoXml.GPO.Name
-            GroupName = $setting.properties.groupName
-            Members   = $user
-            Computer  = $computer
+  process {
+    $output =
+    foreach ($gpp in $gppName) {
+      $guid = (Get-GPO -Name $_).Id
+      [xml]$gpoXml = Get-GPOReport -Guid $guid.Guid -ReportType Xml
+      foreach ($setting in $gpoXml.GPO.Computer.ExtensionData.Extension.LocalUsersAndGroups.Group) {
+        foreach ($user in $setting.properties.members.member.name) {
+          foreach ($computer in $setting.Filters.FilterComputer.name) {
+            [pscustomobject]@{
+              GPO       = $gpoXml.GPO.Name
+              GroupName = $setting.properties.groupName
+              Members   = $user
+              Computer  = $computer
+            }
           }
         }
       }
     }
+    $output | Export-Csv "$($path)\$($fileName)" -NoTypeInformation -Append
   }
-  Return $reportFile
+  end {
+  }
 }
 
 @"
@@ -35,8 +43,5 @@ T2_STD_NA_USA_GBL_RDP-Access_Computer
 T2_STD_LA_RDP-Access_Computer
 T2_STD_EU_RDP-Access_Computer
 T2_STD_AP_AUS_RDP-Access_Computer
-"@ -split [environment]::NewLine | ForEach-Object {
+"@ -split [environment]::NewLine | Get-GPPReport -path 'D:\Temp' -fileName 'GPPReport_14'
 
-  Get-GPPReport -GPPName $_ | Export-Csv "d:\temp\GPPReport.csv" -NoTypeInformation -Append
-
-}
