5764467f25fcffea3171c06ba2ea19fd9c5927fc
Sean Connealy <sjcnyc@outlook.com>
2020-11-17 13:29:42 -0500

    added value from pipeline

--- Printers/Sync-PrintServers.ps1
+++ Printers/Sync-PrintServers.ps1
@@ -1,7 +1,8 @@
-﻿function Sync-PrintServers  {
+﻿function Sync-PrintServers {
+  [CmdletBinding()]
   <#
     .SYNOPSIS
-    Backup, Restore, or Sync Print Servers
+    Backup, Restore, Print Servers
 
     .DESCRIPTION
     For use in the automated backup and restore of print servers
@@ -25,10 +26,10 @@
     .NOTES
     File Name  : Sync-PrintServers.ps1
     Author     : Sean Connealy
-    Requires   : PowerShell Version 3.0 
+    Requires   : PowerShell Version 3.0
     Date       : 1/15/2013
 
-    .LINK 
+    .LINK
     This script posted to: http://www.github/sjcnyc
 
     .INPUTS
@@ -39,42 +40,48 @@
   #>
   param
   (
-    [Parameter(Mandatory)]$PrintServer,
-    [Parameter(Mandatory)][ValidateSet('Backup','Restore', 'Sync')][string]$Operation,
-    [Parameter(Mandatory)]$SavePath,
-    [Parameter(Mandatory)]$File
+    [parameter(ValueFromPipeline = $true, Mandatory = $true, Position = 0)]
+    [string[]]
+    $PrintServer,
+
+    [Parameter(Mandatory)][ValidateSet('Backup', 'Restore')]
+    [string]
+    $Operation,
+
+    [Parameter(Mandatory)]
+    $SavePath,
+
+    [Parameter(Mandatory)]
+    $File
   )
 
-  $currentDate = (get-date -Format MM-dd-yyyy)
+  $currentDate = (Get-Date -Format MM-dd-yyyy)
 
   Set-Location -Path 'C:\Windows\System32\spool\tools' # path to printbrm.exe
 
-  switch($Operation) {
+  switch ($Operation) {
     Backup {
       $PrintServer |
-      ForEach-Object -Process {Start-Process 'printbrm.exe' -ArgumentList "-S \\$($PrintServer) -B -f $($SavePath)\$($File)_$($currentDate).printerExport" -Wait
+      ForEach-Object -Process {
+        Start-Process 'printbrm.exe' -ArgumentList "-S \\$($PrintServer) -B -f $($SavePath)\$($File)_$($currentDate).printerExport" -Wait
       }
     }
     Restore {
       $PrintServer |
-      ForEach-Object -Process {Start-Process 'printbrm.exe' -ArgumentList "-S \\$($PrintServer) -R -f $($SavePath)\$($File).printerExport" -Wait
+      ForEach-Object -Process {
+        Start-Process 'printbrm.exe' -ArgumentList "-S \\$($PrintServer) -R -f $($SavePath)\$($File).printerExport" -Wait
       }
-    }
-    Sync {
-       Write-Verbose -Message 'Not emplemented yet'
     }
   }
 }
 
-
 # Print servers 
 @"
 usnaspwfs01
-"@ -split [environment]::NewLine | ForEach-Object {
+"@ -split [environment]::NewLine |
+Sync-PrintServers -PrintServer $_ -Operation Backup -SavePath '\\storage\infradev$\PrintServer_Backups\' -File $_
 
-  Sync-PrintServers -PrintServer $_ -Operation Backup -SavePath '\\storage\infradev$\PrintServer_Backups\' -File $_
 
-}
 
 #Sync-PrintServers -PrintServer "USDF48E38ABF907" -Operation Backup -SavePath '\\USDF48E38ABF907\c$\Temp' -File 'printer_bak'
 
