33ca3340b3d7b50c9a578d386a082a1c041f38c1
Sean Connealy <sjcnyc@gmail.com>
2021-04-23 18:07:22 -0400

    Add new blob key

--- Az_Scripts/List_AzBlobs.ps1
+++ Az_Scripts/List_AzBlobs.ps1
@@ -1,10 +1,10 @@
 Import-Module Az
 $azStorageAccountName = "sa01sftx3406815508"
-$azStorageAccountKey = "2u6z1WymVz3kYLZZa+xHsLdZSvDYQLXVMN/V27TMUgQe6XKE2bUYyVySBjvC5ps0XHo/nsqSRjufyScGVg7f4Q=="
+$azStorageAccountKey = "6+KFw6fcjw9mhmExSbXrixZMuYz9kOkCAburdJKpmxhuy+ZVmSyeLce9WY8yj6+6nKCwoLqZbbhkfbI4zPKrLg=="
 $azContainerName = "con01-sf-dropoff"
 $azResourceGroupName = "rg-SalesForceTx-prod"
 
-$connectionContext = (Get-AzStorageAccount -ResourceGroupName $azResourceGroupName -AccountName $azStorageAccountName).Context
+$connectionContext = (Get-AzStorageAccount -AccountName $azStorageAccountName).Context
 # Get a list of containers in a storage account
 Get-AzStorageContainer -Name $azStorageAccountName -Context $connectionContext | Select-Object Name
 # Get a list of blobs in a container
