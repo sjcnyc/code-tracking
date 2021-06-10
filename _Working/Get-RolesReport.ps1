4a3ffb820e0fc543c3729f037bf904885c121dcc
Sean Connealy <sjcnyc@outlook.com>
2021-06-10 15:43:55 -0400

    Fix formatting

--- _Working/Get-RolesReport.ps1
+++ _Working/Get-RolesReport.ps1
@@ -8,12 +8,12 @@ function Get-RolesReport {
     $OutDir,
 
     [string]
-    $OutFile = "RolesReport-Tier-$($Tier)-$((Get-Date).ToString("MM.dd.yy-HHmmss")).csv"
+    $OutFile = "RolesReport-Tier-$($Tier)-$((Get-Date).ToString('MM.dd.yy-HHmmss')).csv"
   )
 
   $List = [List[PSObject]]::new()
 
-  $Filter = if ($Tier -eq 3) { "adm*-*" } else { "adm*-$($Tier)" }
+  $Filter = if ($Tier -eq 3) { 'adm*-*' } else { "adm*-$($Tier)" }
 
   $ADUserSplat = @{
     Properties = 'MemberOf', 'Lastlogontimestamp', 'Enabled'
@@ -24,9 +24,9 @@ function Get-RolesReport {
 
   foreach ($User in $Users) {
     switch -Wildcard ($User.Name) {
-      "adm*-2" { $admtier = "Tier-2" }
-      "adm*-1" { $admtier = "Tier-1" }
-      "adm*-0" { $admtier = "Tier-0" }
+      'adm*-2' { $admtier = 'Tier-2' }
+      'adm*-1' { $admtier = 'Tier-1' }
+      'adm*-0' { $admtier = 'Tier-0' }
     }
 
     foreach ($Group in $User.MemberOf) {
@@ -34,37 +34,37 @@ function Get-RolesReport {
         Select-Object Name, DistinguishedName, @{N = 'Manager'; E = { (Get-ADUser -Identity $_.managedBy -Properties Name).Name } })
 
       $RoleAssignment = (($Groups) |
-        Where-Object { $_.Name -like "*-Role" }).Name
+        Where-Object { $_.Name -like '*-Role' }).Name
       $Manager = (($Groups) |
-        Where-Object { $_.Name -like "*-Role" }).Manager
+        Where-Object { $_.Name -like '*-Role' }).Manager
       $NonRoleAssignaments = $Groups |
-        Where-Object { $_.Name -notlike "*-Role" -and $_.Name -notlike "Admin_Tier-*_Users" -and $_.Name -notlike "tier-0_Users" } |
-        Select-Object Name, DistinguishedName
+      Where-Object { $_.Name -notlike '*-Role' -and $_.Name -notlike 'Admin_Tier-*_Users' -and $_.Name -notlike 'tier-0_Users' } |
+      Select-Object Name, DistinguishedName
       $InTierGroup = if (($Groups) |
-        Where-Object { $_.Name -like "Admin_Tier-*_Users" -or $_.Name -like "Tier-0_Users" }) { $true } else { $false }
+        Where-Object { $_.Name -like 'Admin_Tier-*_Users' -or $_.Name -like 'Tier-0_Users' }) { $true } else { $false }
 
       if ($null -ne $NonRoleAssignaments) {
         switch -wildcard ($NonRoleAssignaments.DistinguishedName) {
-          "*OU=NA*" { $owner = "Moldoveanu, Alex" }
-          "*OU=EU*" { $owner = "Elgar, Pete" }
-          "*OU=LA*" { $owner = "Scherer, Pablo" }
-          "*OU=AP*" {
-            if ($NonRoleAssignaments.DistinguishedName -like "*AUS*" -or $NonRoleAssignaments.DistinguishedName -like "*NZL*") {
-              $owner = "McClung, Dustin"
+          '*OU=NA*' { $owner = 'Moldoveanu, Alex' }
+          '*OU=EU*' { $owner = 'Elgar, Pete' }
+          '*OU=LA*' { $owner = 'Scherer, Pablo' }
+          '*OU=AP*' {
+            if ($NonRoleAssignaments.DistinguishedName -like '*AUS*' -or $NonRoleAssignaments.DistinguishedName -like '*NZL*') {
+              $owner = 'McClung, Dustin'
             }
             else {
-              $owner = "Kwan, Ether"
+              $owner = 'Kwan, Ether'
             }
           }
           Default {}
         }
       }
       else {
-        $owner = ""
+        $owner = ''
       }
 
       $PsObj = $PsObj |
-        Where-Object {$NonRoleAssignaments.Name -ne "" -and $RoleAssignments -ne ""}
+      Where-Object { $NonRoleAssignaments.Name -ne '' -and $RoleAssignments -ne '' }
 
       $PsObj = [pscustomobject]@{
         ADMTier            = $admtier
