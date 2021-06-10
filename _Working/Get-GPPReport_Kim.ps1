b3948d3ce5d53383a788a203bbfbe9a5cdced638
Sean Connealy <sjcnyc@outlook.com>
2021-06-10 15:47:43 -0400

    Fix foreach issiue in line 18

--- _Working/Get-GPPReport_Kim.ps1
+++ _Working/Get-GPPReport_Kim.ps1
@@ -3,39 +3,39 @@ function Get-GPPReport {
   param (
     [parameter(ValueFromPipeline = $true, Mandatory = $true, Position = 0)]
     [string[]]
-    $gppName,
+    $GppName,
 
     [parameter(Mandatory = $true, Position = 1)]
-    [string]$path,
+    [string]$Path,
 
     [parameter(Mandatory = $true, Position = 2)]
-    [string]$fileName
+    [string]$FileName
   )
 
-  begin { $fileName = "$($fileName)_$(Get-Date -Format {MMddyyyy-HHmmss}).csv" }
+  begin { $FileName = "$($FileName)_$(Get-Date -Format {MMddyyyy}).csv" }
 
   process {
-    $output =
-    foreach ($gpp in $gppName) {
-      $guid = (Get-GPO -Name $_).Id
-      [xml]$gpoXml = Get-GPOReport -Guid $guid.Guid -ReportType Xml
-      foreach ($setting in $gpoXml.GPO.Computer.ExtensionData.Extension.LocalUsersAndGroups.Group) {
-        foreach ($user in $setting.properties.members.member.name) {
-          foreach ($computer in $setting.Filters.FilterComputer.name) {
+    foreach ($Gpp in $GppName) {
+      $Guid = (Get-GPO -Name $Gpp).Id
+      [xml]$GpoXml = Get-GPOReport -Guid $Guid -ReportType Xml
+      foreach ($Setting in $GpoXml.GPO.Computer.ExtensionData.Extension.LocalUsersAndGroups.Group) {
+        foreach ($User in $Setting.properties.members.member.name) {
+          $Output =
+          foreach ($Computer in $Setting.Filters.FilterComputer.name) {
             [pscustomobject]@{
-              GPO       = $gpoXml.GPO.Name
-              GroupName = $setting.properties.groupName
-              Members   = $user
-              Computer  = $computer
+              GPO       = $GpoXml.GPO.Name
+              GroupName = $Setting.properties.groupName
+              Members   = $User
+              Computer  = $Computer
             }
           }
+          $Output | Export-Csv "$($Path)\$($FileName)" -NoTypeInformation -Append
         }
       }
     }
-    $output | Export-Csv "$($path)\$($fileName)" -NoTypeInformation -Append
-    #Write-Verbose $output.message
   }
   end {
+    #$Output | Export-Csv "$($Path)\$($FileName)" -NoTypeInformation
   }
 }
 
@@ -46,5 +46,3 @@ T2_STD_EU_RDP-Access_Computer
 T2_STD_AP_AUS_RDP-Access_Computer
 T2_STD_Computer
 '@ -split [environment]::NewLine | Get-GPPReport -path 'D:\Temp' -fileName 'GPPReport' -Verbose
-
-
