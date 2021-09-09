699d70b6aca51c8f2b72d94f4db75f387290cb5f
Sean Connealy <sjcnyc@outlook.com>
2021-09-09 12:11:14 -0400

    Fix: no fix lol

--- Functions/out-notepad2.ps1
+++ Functions/out-notepad2.ps1
@@ -5,7 +5,7 @@
     [Object]
     [AllowEmptyString()]
     $Object,
- 
+
     [Int]
     $Width = 150
   )
@@ -13,7 +13,7 @@
   begin {
     $al = New-Object System.Collections.ArrayList
   }
- 
+
   process {
     [void] $al.Add($Object)
   }
@@ -22,20 +22,22 @@
     Format-Table -AutoSize -Wrap |
     Out-String -Width $Width
  
-    $process = Start-Process "notepad" -PassThru
-    $notepad = $process.Handle
+    $process = Start-Process 'C:\Program Files\Notepad3\Notepad3.exe' -PassThru
+    $notepad = $process.MainWindowHandle
     [void] $process.WaitForInputIdle()
  
     $sig = '
       [DllImport("user32.dll", EntryPoint = "FindWindowEx")]public static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
       [DllImport("user32.dll")]public static extern int SendMessage(IntPtr hWnd, int uMsg, int wParam, string lParam);
     '
- 
+
     $type = Add-Type -MemberDefinition $sig -Name APISendMessage2 -PassThru
     $hwnd = $notepad
+    $WM_SETTEXT = '0x000C'
+    $test1 = "this is a test"
 
     [IntPtr]$child = $type::FindWindowEx($hwnd, [IntPtr]::Zero, 'Edit', $null)
-    [void]$type::SendMessage($child, 0x000C, 0, $text)
+    [void]$type::SendMessage($child, $WM_SETTEXT, [IntPtr]::Zero, $text1)
   }
 }
 
