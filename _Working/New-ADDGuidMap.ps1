615d53d0a366f88515f31b84c8cb97fd13b6c2cc
Sean Connealy <sjcnyc@outlook.com>
2021-09-09 12:14:00 -0400

    first commit

--- _Working/New-ADDGuidMap.ps1
+++ _Working/New-ADDGuidMap.ps1
@@ -1,4 +1,5 @@
 function New-ADDGuidMap {
+  <#
   .SYNOPSIS
   Creates a guid map for the delegation part
   .DESCRIPTION
@@ -20,4 +21,27 @@ function New-ADDGuidMap {
   }
   Get-ADObject @guidMapParams | ForEach-Object { $guidmap[$_.lDAPDisplayName] = [System.GUID]$_.schemaIDGUID }
   return $guidmap
+}
+
+function New-ADDExtendedRightsMap {
+  <#
+  .SYNOPSIS
+  Creates a extended rights map for the delegation part
+  .DESCRIPTION
+  Creates a extended rights map for the delegation part
+  .EXAMPLE
+  Get-Process C:\> New-ADDExtendedRightsMap
+  .NOTES
+  Author: Constantin Hager
+  Date: 06.08.2019
+  #>
+  $rootdse = Get-ADRootDSE
+  $ExtendedMapParams = @{
+    SearchBase = ($rootdse.ConfigurationNamingContext)
+    LDAPFilter = '(&(objectclass=controlAccessRight)(rightsguid=*))'
+    Properties = ('displayName', 'rightsGuid')
+  }
+  $extendedrightsmap = @{ }
+  Get-ADObject @ExtendedMapParams | ForEach-Object { $extendedrightsmap[$_.displayName] = [System.GUID]$_.rightsGuid }
+  return $extendedrightsmap
 }
