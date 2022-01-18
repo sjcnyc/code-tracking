5ddf9bee1f5e543c6f9406d28bc9a0e915f01243
Sean Connealy <sjcnyc@outlook.com>
2022-01-18 12:57:40 -0500

    Comment out  quiet switch

--- _Working/Update-PowerShellVersion.ps1
+++ _Working/Update-PowerShellVersion.ps1
@@ -1,1 +1,1 @@
-Invoke-Expression "& { $(Invoke-RestMethod 'https://aka.ms/install-powershell.ps1') } –useMSI -EnablePSRemoting -Quiet"
+Invoke-Expression "& { $(Invoke-RestMethod 'https://aka.ms/install-powershell.ps1') } –useMSI -EnablePSRemoting" # -Quiet"
