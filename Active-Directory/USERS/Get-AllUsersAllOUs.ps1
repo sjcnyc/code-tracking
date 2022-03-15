9f66e235f17437205cdd88384f9163ab71c9a1a9
Sean Connealy <sjcnyc@outlook.com>
2022-03-15 09:49:51 -0400

    Syntax fixes

--- Active-Directory/USERS/Get-AllUsersAllOUs.ps1
+++ Active-Directory/USERS/Get-AllUsersAllOUs.ps1
@@ -1,25 +1,25 @@
 ﻿
 
 Get-QADUser -IncludeAllProperties -SizeLimit 0 -SearchScope Subtree -Service me.sonymusic.com |
-Select-Object SAMAccountName, Displayname,useraccountcontrol,distinguishedname, parentcontainer, `
-	 @{N='AccountStatus';E={if ($_.AccountIsDisabled -eq 'TRUE'){'Disabled'}else{'Enabled'}}} |
-		Export-Csv -Path 'c:\temp\me_user_report_3.csv' -NoTypeInformation
+Select-Object SAMAccountName, Displayname, useraccountcontrol, distinguishedname, parentcontainer, `
+@{N = 'AccountStatus'; E = { if ($_.AccountIsDisabled -eq 'TRUE') { 'Disabled' }else { 'Enabled' } } } |
+Export-Csv -Path 'c:\temp\me_user_report_3.csv' -NoTypeInformation
 
 
 
-@"
+@'
 DC=me,DC=sonymusic,DC=com
-"@ -split [environment]::NewLine | ForEach-Object {
+'@ -split [environment]::NewLine | ForEach-Object {
 
   $Filter = [RegEx]'^*OU=Employee*|^*OU=Non-Employee'
 
   $getADUserSplat = @{
     SearchBase = $_
-    Properties = 'SamAccountName', 'Name', 'DistinguishedName'
-    Server     = "me.sonymusic.com"
+    Properties = 'SamAccountName', 'Name', 'Enabled'
+    Server     = 'me.sonymusic.com'
     Filter     = '*'
   }
 
-  Get-ADUser @getADUserSplat |Where-Object {$_.DistinguishedName -match $Filter} |Select-Object SAMAccountName, Name, DistinguishedName |Export-Csv -Path 'c:\temp\ME_Admin_Report.csv' -NoTypeInformation -Append
+  Get-ADUser @getADUserSplat | Where-Object { $_.DistinguishedName -match $Filter } | Select-Object SAMAccountName, Name, DistinguishedName | Export-Csv -Path 'c:\temp\ME_Admin_Report.csv' -NoTypeInformation -Append
 
 }
