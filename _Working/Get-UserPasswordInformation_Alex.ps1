beaa7a21b0a87788d7111c5920419d8779f56cf6
Sean Connealy <sjcnyc@gmail.com>
2022-05-17 15:56:14 -0400

    Re-format

--- _Working/Get-UserPasswordInformation_Alex.ps1
+++ _Working/Get-UserPasswordInformation_Alex.ps1
@@ -4,14 +4,14 @@ OU=Non-Employees,OU=Users,OU=RIO,OU=BRA,OU=LA,OU=STD,OU=Tier-2,DC=me,DC=sonymusi
 '@ -split [environment]::newline
 
 $Users =
-    foreach ($OU in $OUs) {
-        $getADUserSplat = @{
-            Filter     = '*'
-            SearchBase = $ou
-            Properties = 'GivenName', 'Surname', 'Samaccountname', 'DistinguishedName', 'PasswordNeverExpires', 'PasswordNotRequired', 'Enabled', 'PasswordLastSet', 'PasswordExpired'
-        }
-
-        Get-ADUser @getADUserSplat | Select-Object $getADUserSplat.Properties
+foreach ($OU in $OUs) {
+    $getADUserSplat = @{
+        Filter     = '*'
+        SearchBase = $ou
+        Properties = 'GivenName', 'Surname', 'Samaccountname', 'DistinguishedName', 'PasswordNeverExpires', 'PasswordNotRequired', 'Enabled', 'PasswordLastSet', 'PasswordExpired'
     }
+
+    Get-ADUser @getADUserSplat | Select-Object $getADUserSplat.Properties
+}
 
 $Users | Export-Csv D:\Downloads\report3.csv -NoTypeInformation
