1ee3a27922c80b0304c0fdd4cc1662de10b26a7e
Sean Connealy <sjcnyc@gmail.com>
2022-05-21 22:11:12 -0400

    Splat params for arg completer

--- Az-Scripts/Add-UserToAvdGroup.ps1
+++ Az-Scripts/Add-UserToAvdGroup.ps1
@@ -3,14 +3,13 @@ function Add-UserToAvdGroup {
     param(
         [Parameter(Mandatory = $true)]
         [string]$sAMAccountName,
-
         [Parameter(Mandatory = $true)]
         [string]$Groups
     )
     try {
         foreach ($Group in $Groups) {
             $GroupDN = (Get-ADGroup -Identity $Group).DistinguishedName
-            if (-not (Get-ADGroupMember -Identity $GroupDN | Where-Object {$_.sAMAccountName -eq $sAMAccountName})) {
+            if (-not (Get-ADGroupMember -Identity $GroupDN | Where-Object { $_.sAMAccountName -eq $sAMAccountName })) {
                 Add-ADGroupMember -Identity $GroupDN -Members $sAMAccountName
                 Write-Host "Adding $($sAMAccountName) to AVD group: $($Group)" -ForegroundColor Green
             } else {
@@ -21,9 +20,11 @@ function Add-UserToAvdGroup {
         $_.Exception.message
     }
 }
-
 Register-ArgumentCompleter -CommandName 'Add-UserToAvdGroup' -ParameterName 'Groups' -ScriptBlock {
     param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters)
-
-    (Get-ADGroup -Filter 'Name -like "az_avd_*FullDesktop"' -prop Name).Name | ForEach-Object { "'$_'" }
+    $Parameters = @{
+        Filter = 'Name -like "az_avd_*FullDesktop" -and Name -notlike "*Tier*"'
+        prop   = 'Name'
+    }
+    (Get-ADGroup @Parameters).Name | ForEach-Object { "'$_'" }
 }
