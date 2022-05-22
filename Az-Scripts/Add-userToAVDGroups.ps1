5d84dd6ca5f9188263d96ee677a5497195bf793f
Sean Connealy <sjcnyc@outlook.com>
2022-05-21 22:16:52 -0400

    Re -worked

---
+++ Az-Scripts/Add-userToAVDGroups.ps1
@@ -0,0 +1,30 @@
+function Add-UserToAvdGroup {
+    [CmdletBinding(SupportsShouldProcess = $true)]
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]$sAMAccountName,
+        [Parameter(Mandatory = $true)]
+        [string]$Groups
+    )
+    try {
+        foreach ($Group in $Groups) {
+            $GroupDN = (Get-ADGroup -Identity $Group).DistinguishedName
+            if (-not (Get-ADGroupMember -Identity $GroupDN | Where-Object { $_.sAMAccountName -eq $sAMAccountName })) {
+                Add-ADGroupMember -Identity $GroupDN -Members $sAMAccountName
+                Write-Host "Adding $($sAMAccountName) to AVD group: $($Group)" -ForegroundColor Green
+            } else {
+                Write-Host "User $($sAMAccountName) already in AVD group: $($Group)" -ForegroundColor Red
+            }
+        }
+    } catch {
+        $_.Exception.message
+    }
+}
+Register-ArgumentCompleter -CommandName 'Add-UserToAvdGroup' -ParameterName 'Groups' -ScriptBlock {
+    param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters)
+    $Parameters = @{
+        Filter = 'Name -like "az_avd_*FullDesktop" -and Name -notlike "*Tier*"'
+        prop   = 'Name'
+    }
+    (Get-ADGroup @Parameters).Name | ForEach-Object { "'$_'" }
+}
