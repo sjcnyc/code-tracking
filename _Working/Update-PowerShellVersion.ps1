0cda1142bb9561788d000c2d2664bf0c06bcbefa
Sean Connealy <sjcnyc@outlook.com>
2022-06-19 11:22:18 -0400

    Splatted commands
    Signed-off-by: Sean Connealy <sjcnyc@outlook.com>

--- _Working/Update-PowerShellVersion.ps1
+++ _Working/Update-PowerShellVersion.ps1
@@ -1,1 +1,5 @@
-Invoke-Expression "& { $(Invoke-RestMethod 'https://aka.ms/install-powershell.ps1') } –useMSI -EnablePSRemoting" # -Quiet"
+$invokeExpressionSplat = @{
+    Command = "& { $(Invoke-RestMethod 'https://aka.ms/install-powershell.ps1')  -useMSI -EnablePSRemoting"
+}
+
+Invoke-Expression @invokeExpressionSplat
