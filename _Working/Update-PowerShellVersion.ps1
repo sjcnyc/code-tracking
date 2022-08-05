da00988402aafca135257103d9637e51a220650a
Sean Connealy <sjcnyc@outlook.com>
2022-08-05 13:17:34 -0400

    Added iex
    Signed-off-by: Sean Connealy <sjcnyc@outlook.com>

--- _Working/Update-PowerShellVersion.ps1
+++ _Working/Update-PowerShellVersion.ps1
@@ -1,5 +1,1 @@
-$invokeExpressionSplat = @{
-    Command = "& { $(Invoke-RestMethod 'https://aka.ms/install-powershell.ps1')  -useMSI -EnablePSRemoting"
-}
-
-Invoke-Expression @invokeExpressionSplat
+& { $(Invoke-RestMethod 'https://aka.ms/install-powershell.ps1') -useMSI -EnablePSRemoting -Quiet }
