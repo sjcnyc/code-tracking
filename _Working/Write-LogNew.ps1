aa9f0f642fd227e1a7c533d5332e7468cbfb6d8a
Sean Connealy <sjcnyc@gmail.com>
2023-05-23 09:33:49 -0400

    First commit
    Signed-off-by: Sean Connealy <sjcnyc@gmail.com>

---
+++ _Working/Write-LogNew.ps1
@@ -0,0 +1,41 @@
+function Write-LogNew {
+       param (
+              [string[]]
+              $Message,
+
+              [string]
+              $LogFile = $Script:LogFile,
+
+              [switch]
+              $ConsoleOutput,
+
+              [ValidateSet("SUCCESS", "INFO", "WARN", "ERROR", "DEBUG")]
+              [string]
+              $LogLevel
+       )
+       $Message = $Message + $Input
+       if (!$LogLevel) { $LogLevel = "INFO" }
+       switch ($LogLevel) {
+              SUCCESS { $Color = "Green" }
+              INFO { $Color = "White" }
+              WARN { $Color = "Yellow" }
+              ERROR { $Color = "Red" }
+              DEBUG { $Color = "Gray" }
+       }
+       if ($unll -ne $Message -and $Message.Length -gt 0) {
+              $TimeStamp = [System.DateTime]::Now.ToString("yyyy-MM-dd HH:mm:ss")
+              if ($LogFile -ne $null -and $LogFile -ne [System.String]::Empty) {
+                     Out-File -Append -FilePath $LogFile -InputObject "[$TimeStamp] [$LogLevel] $Message"
+              }
+              if ($ConsoleOutput -eq $true) {
+                     Write-Host "[$TimeStamp] [$LogLevel] :: $Message" -ForegroundColor $Color
+
+                     if ($AutomationPSConnection -or $AutomationPSCertificate) {
+                            Write-Output "[$TimeStamp] [$LogLevel] :: $Message"
+                     }
+              }
+              if ($LogLevel -eq "ERROR") {
+                     Write-Error "[$TimeStamp] [$LogLevel] :: $Message"
+              }
+       }
+}
