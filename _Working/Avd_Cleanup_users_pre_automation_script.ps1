1e8c9b95626ff774c328b3c54f64e1e0ecaeff63
Sean <sjcnyc@gmail.com>
2024-02-05 13:41:40 -0500

    Working script for avd cleanup
    Signed-off-by: Sean <sjcnyc@gmail.com>

---
+++ _Working/Avd_Cleanup_users_pre_automation_script.ps1
@@ -0,0 +1,6 @@
+$users = Import-Csv C:\Temp\avd_remove_list.csv
+
+foreach ($user in $users){
+    #Write-Output "$($user.SamaccountName) in: $($user.GroupName)"
+    Remove-ADGroupMember -Identity $user.GroupName -Members $user.SamaccountName -Confirm:$false -whatif
+}
