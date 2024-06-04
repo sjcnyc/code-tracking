854080b53e6eb6966b4b0b8aedefcffb02de0eb9
Sean Connealy <sjcnyc@gmail.com>
2024-06-04 12:58:57 -0400

    added argument completer notes
    Signed-off-by: Sean Connealy <sjcnyc@gmail.com>

--- Az-Scripts/Add-userToAVDGroups.ps1
+++ Az-Scripts/Add-userToAVDGroups.ps1
@@ -1,3 +1,27 @@
+<#
+.SYNOPSIS
+Adds a user to Azure Virtual Desktop (AVD) groups.
+
+.DESCRIPTION
+The Add-UserToAvdGroup function adds a specified user to one or more AVD groups. It checks if the user is already a member of the group before adding them.
+
+.PARAMETER sAMAccountName
+The sAMAccountName of the user to be added to the AVD groups.
+
+.PARAMETER Groups
+The AVD groups to which the user should be added. This parameter accepts an array of group names.
+
+.EXAMPLE
+Add-UserToAvdGroup -sAMAccountName "john.doe" -Groups <tab for argument completer to list available groups>
+Adds the user with sAMAccountName "john.doe" to the AVD groups specified.
+
+.NOTES
+Author: Sean Connealy
+Date:   2021-09-01
+Script: Add-UserToAvdGroup.ps1
+pre-requisite: AzureAD module
+#>
+
 function Add-UserToAvdGroup {
     [CmdletBinding(SupportsShouldProcess = $true)]
     param(
@@ -20,6 +44,7 @@ function Add-UserToAvdGroup {
         $_.Exception.message
     }
 }
+
 Register-ArgumentCompleter -CommandName 'Add-UserToAvdGroup' -ParameterName 'Groups' -ScriptBlock {
     param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters)
     $Parameters = @{
