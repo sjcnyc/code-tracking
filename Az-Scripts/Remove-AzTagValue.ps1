7972ecc7f943d5b14e2aa88d6510b137b2cdaf30
Sean Connealy <sjcnyc@gmail.com>
2024-06-04 08:53:54 -0400

    Add comments
    Signed-off-by: Sean Connealy <sjcnyc@gmail.com>

--- Az-Scripts/Remove-AzTagValue.ps1
+++ Az-Scripts/Remove-AzTagValue.ps1
@@ -1,3 +1,49 @@
+<#
+.SYNOPSIS
+    Removes Azure resource tags based on tag name, tag value, or tag object.
+
+.DESCRIPTION
+    The Remove-AzureTagValue function removes Azure resource tags based on the specified tag name, tag value, or tag object.
+    It supports removing tags from both resource groups and individual resources.
+
+.PARAMETER SubscriptionName
+    Specifies the name of the Azure subscription to use.
+
+.PARAMETER TagName
+    Specifies the name of the tag to remove. This parameter is used when removing tags based on tag name or tag value.
+
+.PARAMETER TagValue
+    Specifies the value of the tag to remove. This parameter is used when removing tags based on tag value.
+
+.PARAMETER Tag
+    Specifies the tag object to remove. This parameter is used when removing tags based on a tag object.
+
+.EXAMPLE
+    Remove-AzureTagValue -SubscriptionName "MySubscription" -TagName "Environment"
+
+    This example removes all tags with the name "Environment" from all resource groups and resources in the specified Azure subscription.
+
+.EXAMPLE
+    Remove-AzureTagValue -SubscriptionName "MySubscription" -TagName "Environment" -TagValue "Production"
+
+    This example removes all tags with the name "Environment" and the value "Production" from all resource groups and resources in the specified Azure subscription.
+
+.EXAMPLE
+    $tag = @{
+        Name = "Environment"
+        Value = "Staging"
+    }
+    Remove-AzureTagValue -SubscriptionName "MySubscription" -Tag $tag
+
+    This example removes the specified tag object from all resource groups and resources in the specified Azure subscription.
+
+.NOTES
+File Name      : Remove-AzTagValue.ps1
+Author         : Sean Connealy
+Prerequisite   : Azure PowerShell module
+Requirements   : PowerShell 5.1 or later
+#>
+
 function Remove-AzureTagValue {
 
     [CmdletBinding()]
