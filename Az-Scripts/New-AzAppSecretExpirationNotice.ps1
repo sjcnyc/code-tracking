62455e0fe9baee20b8a6d67b5ede33854f348900
Sean Connealy <sjcnyc@gmail.com>
2024-12-10 10:15:54 -0500

    Add New-AzAppSecretExpirationNotice

---
+++ Az-Scripts/New-AzAppSecretExpirationNotice.ps1
@@ -0,0 +1,155 @@
+$AppID = '91152ce4-ea23-4c83-852e-05e564545fb9'
+$TenantID = 'f0aff3b7-91a5-4aae-af71-c63e1dda2049'
+$AppSecret = 'NGi8Q~V5dRHJ6_7WRxHjKrNhI0VFJtqTk7xn_ay-'
+
+[int32]$expirationDays = 5
+[string]$emailSender = "pwshalerts@sonymusic.com"
+[string]$emailTo = "sean.connealy.ext@sonymusic.com"
+
+
+Function Connect-MSGraphAPI {
+    param (
+        [system.string]$AppID,
+        [system.string]$TenantID,
+        [system.string]$AppSecret
+    )
+    begin {
+        $URI = "https://login.microsoftonline.com/$TenantID/oauth2/v2.0/token"
+        $ReqTokenBody = @{
+            Grant_Type    = "client_credentials"
+            Scope         = "https://graph.microsoft.com/.default"
+            client_Id     = $AppID
+            Client_Secret = $AppSecret
+        }
+    }
+    Process {
+        Write-Output "Connecting to the Graph API"
+        $Response = Invoke-RestMethod -Uri $URI -Method POST -Body $ReqTokenBody
+    }
+    End {
+        $Response
+    }
+}
+
+Function Get-MSGraphRequest {
+    param (
+        [system.string]$Uri,
+        [system.string]$AccessToken
+    )
+    begin {
+        [System.Array]$allPages = @()
+        $ReqTokenBody = @{
+            Headers = @{
+                "Content-Type"  = "application/json"
+                "Authorization" = "Bearer $($AccessToken)"
+            }
+            Method  = "Get"
+            Uri     = $Uri
+        }
+    }
+    process {
+        write-verbose "GET request at endpoint: $Uri"
+        $data = Invoke-RestMethod @ReqTokenBody
+        while ($data.'@odata.nextLink') {
+            $allPages += $data.value
+            $ReqTokenBody.Uri = $data.'@odata.nextLink'
+            $Data = Invoke-RestMethod @ReqTokenBody
+            # to avoid throttling, the loop will sleep for 3 seconds
+            Start-Sleep -Seconds 3
+        }
+        $allPages += $data.value
+    }
+    end {
+        Write-Verbose "Returning all results"
+        $allPages
+    }
+}
+
+Function Send-MSGraphEmail {
+    param (
+        [system.string]$Uri,
+        [system.string]$AccessToken,
+        [system.string]$To,
+        [system.string]$Subject = "App Secret Expiration Notice",
+        [system.string]$Body
+    )
+    begin {
+        $headers = @{
+            "Authorization" = "Bearer $($AccessToken)"
+            "Content-type"  = "application/json"
+        }
+
+        $BodyJsonsend = @"
+{
+   "message": {
+   "subject": "$Subject",
+   "body": {
+      "contentType": "HTML",
+      "content": "$($Body)"
+   },
+   "toRecipients": [
+      {
+      "emailAddress": {
+      "address": "$to"
+          }
+      }
+   ]
+   },
+   "saveToSentItems": "true"
+}
+"@
+    }
+    process {
+        $data = Invoke-RestMethod -Method POST -Uri $Uri -Headers $headers -Body $BodyJsonsend
+    }
+    end {
+        $data
+    }
+}
+
+$tokenResponse = Connect-MSGraphAPI -AppID $AppID -TenantID $TenantID -AppSecret $AppSecret
+
+$array = @()
+$apps = Get-MSGraphRequest -AccessToken $tokenResponse.access_token -Uri "https://graph.microsoft.com/v1.0/applications/"
+foreach ($app in $apps) {
+    $app.passwordCredentials | foreach-object {
+        #If there is a secret with a enddatetime, we need to get the expiration of each one
+        if ($_.endDateTime -ne $null) {
+            [system.string]$secretdisplayName = $_.displayName
+            [system.string]$id = $app.id
+            [system.string]$displayname = $app.displayName
+            $Date = [TimeZoneInfo]::ConvertTimeBySystemTimeZoneId($_.endDateTime, 'Central Standard Time')
+            [int32]$daysUntilExpiration = (New-TimeSpan -Start ([System.TimeZoneInfo]::ConvertTimeBySystemTimeZoneId([DateTime]::Now, "Central Standard Time")) -End $Date).Days
+
+            if (($daysUntilExpiration -ne $null) -and ($daysUntilExpiration -le $expirationDays)) {
+                $array += $_ | Select-Object @{
+                    name = "id";
+                    expr = { $id }
+                },
+                @{
+                    name = "displayName";
+                    expr = { $displayName }
+                },
+                @{
+                    name = "secretName";
+                    expr = { $secretdisplayName }
+                },
+                @{
+                    name = "daysUntil";
+                    expr = { $daysUntilExpiration }
+                }
+            }
+            $daysUntilExpiration = $null
+            $secretdisplayName = $null
+        }
+    }
+}
+
+if ($array -ne 0) {
+    write-output "sending email"
+    $textTable = $array | Sort-Object daysUntil | Select-Object displayName, secretName, daysUntil | ConvertTo-Html -Fragment
+    Send-MSGraphEmail -Uri "https://graph.microsoft.com/v1.0/users/$emailSender/sendMail" -AccessToken $tokenResponse.access_token -To $emailTo  -Body $textTable
+}
+else {
+    write-output "No apps with expiring secrets"
+}
